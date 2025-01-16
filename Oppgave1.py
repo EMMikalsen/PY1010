a96b4752aa9141aa496703976377d2f03ddbb1b0
EMMikalsen <evelyn.margrethe.mikalsen@gmail.com>
2025-01-16 18:04:26 +0100

    Add files via upload

---
+++ Oppgave1.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jan 11 20:55:03 2025
+
+@author: EMM
+"""
+
+iÅrNå = 2024
+alder = int(input('Hvilket år er du født? ') )
+
+iAlderIÅr = iÅrNå - alder
+
+print("Du er", iAlderIÅr, "år i", iÅrNå)
