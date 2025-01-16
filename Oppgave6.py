45a4fa90d3ca049e80572169f65353638e128496
EMMikalsen <evelyn.margrethe.mikalsen@gmail.com>
2025-01-16 22:48:11 +0100

    Add files via upload

---
+++ Oppgave6.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jan 16 22:02:57 2025
+
+@author: EMM
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+x = np.linspace(-10, 10, 200)
+y = -x ** 2 - 5
+
+plt.plot(x,y)
